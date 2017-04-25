3284e83d3d990c555e84c97b6fd843ff343344db
naesheim <hg.nesheim@gmail.com>
2017-04-25 14:25:22 +0200

    add prometheus and grafana for monitoring

---
+++ Grafana/config/init_datasource.sh
@@ -0,0 +1,14 @@
+set -e
+
+DS_NAME=Prom
+DS_TYPE==Prometheus
+DS_ACCESS=proxy
+DS_URL=http://prometheus:9090
+DS_AUTH=false
+DS_ISDEFAULT=true
+
+curl -u admin:admin \
+'http://localhost:3000/api/datasources' \
+-X POST \
+-H 'Content-Type: application/json;charset=UTF-8' \
+--data-binary '{"name":"Prometheus","type":"prometheus","url":"http://prometheus:9090","access":"proxy","basicauth":false}'
